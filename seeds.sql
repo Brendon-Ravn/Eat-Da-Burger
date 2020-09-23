59a3992606ed864aa4b3ec12e8bb4bdbce7e3f96
Brendon Conatser <b.conatser21@gmail.com>
2020-09-23 10:21:27 -0500

    initial commit

---
+++ seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Black and Blue", true);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Breakfast Burger", true);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Homestyle Cheese Burger", true);
