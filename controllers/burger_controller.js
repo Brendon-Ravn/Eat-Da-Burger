468f3c02d5b853074322d5fd5cbf7a1c87b720f7
Brendon Conatser <b.conatser21@gmail.com>
2020-09-24 10:56:08 -0500

    fixed routing issues

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var burger = require("../models/burger.js")
 
-var router = express()
+var router = express.Router()
 
 router.get("/", function(req, res) {
   burger.all(function(data) {
